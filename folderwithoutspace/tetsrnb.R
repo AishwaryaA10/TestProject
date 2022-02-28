12b9a297499df2f8a914c7d36c4b05afd9c7481f
refract_user <refract_user@lntinfotech.com>
2022-02-28 10:06:08 +0000

    manual

--- folderwithoutspace/tetsrnb.R
+++ folderwithoutspace/tetsrnb.R
@@ -3,3 +3,9 @@
 # A number is even if division by 2 give a remainder of 0.
 # If remainder is 1, it is odd.
 num = as.integer(readline(prompt="Enter a number: "))
+#manual
+if((num %% 2) == 0) {
+  print(paste(num,"is Even"))
+} else {
+  print(paste(num,"is Odd"))
+}
