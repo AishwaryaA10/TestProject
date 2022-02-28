631e60b9b4eb6a967bd18001546496fab0f51f47
refract_user <refract_user@lntinfotech.com>
2022-02-28 08:32:00 +0000

    auto save by knights-watch

---
+++ Folder with space/filw with space.R
@@ -0,0 +1,6 @@
+#manual
+# Program to check if the input number is prime or not
+# take input from the user
+num = as.integer(readline(prompt="Enter a number: "))
+flag = 0
+# prime numbers are greater than 1
