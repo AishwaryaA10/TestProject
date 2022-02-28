d756ec8d36bac49b652a89842ee63d4e999611bf
refract_user <refract_user@lntinfotech.com>
2022-02-28 10:01:48 +0000

    auto save by knights-watch

---
+++ folderwithoutspace/tetsrnb.R
@@ -0,0 +1,5 @@
+# auto 
+# Program to check if the input number is odd or even.
+# A number is even if division by 2 give a remainder of 0.
+# If remainder is 1, it is odd.
+num = as.integer(readline(prompt="Enter a number: "))
